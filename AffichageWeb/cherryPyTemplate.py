ba70170fa3d05a41c40d8e194c5b9e7b6c738e9b
Jonas BOUSCHARAIN <E134188G@U-INF-J-E114-18.iut-nantes.univ-nantes.prive>
2015-03-26 11:17:55 +0100

    Mise en place du service web

--- AffichageWeb/cherryPyTemplate.py
+++ AffichageWeb/cherryPyTemplate.py
@@ -17,68 +17,139 @@ class WebManager(object):
         """
         Exposes the service at localhost:8080/
         """
-        view = Template(filename="template.html", lookup=lookup)
-
-        return view.render(
-		rows=[[item["name"], item["age"]] for item in data]
-	)
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <br></br>
+                    <a href='show_activite'>Table d'activités</a></br>
+                    <a href='show_equipement'>Table d'equipements<a/></br>
+                    <a href='show_installation'>Table d'installations<a/>
+                    """
+        return maPage
 
     @cherrypy.expose
     def show_equipement(self):
         """
         Exposes the service at localhost:8080/show_equipement/
         """
-        conn = sqlite3.connect("dataBase.db")
-        c = conn.cursor()
-        res="<table border='1' style='text-align'><th>Nom</th><th>ID Equipement</th><th>ID Installation</th><tr>"
-        for row in c.execute('SELECT * FROM equipement ORDER BY EquNom'):
-            for elem in row:
-                res = res + "<td>" + str(elem) + "</td>"
-            res = res + "</tr>"
-        res = res + "</table>"
-        return res
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'équipements sportif</h2>
+                    <form name='monForm' action='show_equipement_id'>
+                        <input type="text" name='id'>
+                        <input type="submit">
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
+        return maPage
 
     @cherrypy.expose
     def show_activite(self):
         """
         Exposes the service at localhost:8080/show_equipement/
         """
-        conn = sqlite3.connect("dataBase.db")
-        c = conn.cursor()
-        res="<table border='1' style='text-align'><th>Nom</th><th>ID Activite</th><th>ID Equipement</th><tr>"
-        for row in c.execute('SELECT * FROM activite ORDER BY ActLib'):
-            for elem in row:
-                res = res + "<td>" + str(elem) + "</td>"
-            res = res + "</tr>"
-        res = res + "</table>"
-        return res
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'équipements sportif</h2>
+                    <form name='monForm' action='show_activite_id'>
+                        <input type="text" name='id'>
+                        <input type="submit">
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
+        return maPage
 
     @cherrypy.expose
     def show_installation(self):
         """
         Exposes the service at localhost:8080/show_equipement/
         """
-        conn = sqlite3.connect("dataBase.db")
-        c = conn.cursor()
-        res="<table border='1'><th>Ville</th><th>Code Postal</th><th>Adresse</th><th>ID Installation</th><th>Nom Installation</th><th>Latitude</th><th>Longitude</th><tr>"
-        for row in c.execute('SELECT * FROM installation ORDER BY InsCodePostal'):
-            for elem in row:
-                res = res + "<td>" + str(elem) + "</td>"
-            res = res + "</tr>"
-        res = res + "</table>"
-        return res
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'équipements sportif</h2>
+                    <form name='monForm' action='show_installation_id'>
+                        <input type="text" name='id'>
+                        <input type="submit" id='submit'>
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
+        return maPage
 
     @cherrypy.expose
-    def show(self, id):
+    def show_equipement_id(self, id):
         """
         Exposes the service at localhost:8080/show/[id]/
         """
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'équipements sportif</h2>
+                    <form name='monForm' action='show_equipement_id'>
+                        <input type="text" name='id'>
+                        <input type="submit">
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
         try:
-            item = data[int(id)]
+            conn = sqlite3.connect("dataBase.db")
+            c = conn.cursor()
+            maPage +="<table border='1' style='text-align'><th>Nom</th><th>ID Equipement</th><th>ID Installation</th><tr>"
+            for row in c.execute("SELECT * FROM equipement WHERE EquipementId LIKE \'%"+id+"%\'"):
+                for elem in row:
+                    maPage += "<td>" + str(elem) + "</td>"
+                maPage += "</tr>"
+            maPage += "</table>"
         except (IndexError, IOError):
             return "Invalid ID"
 
-        return json.dumps(item)
+        return maPage
+
+    @cherrypy.expose
+    def show_installation_id(self, id):
+        """
+        Exposes the service at localhost:8080/show/[id]/
+        """
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'installations sportives</h2>
+                    <form name='monForm' action='show_installation_id'>
+                        <input type="text" name='id'>
+                        <input type="submit">
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
+        try:
+            conn = sqlite3.connect("dataBase.db")
+            c = conn.cursor()
+            maPage +="<table border='1' style='text-align'><th>Nom</th><th>ID Equipement</th><th>ID Installation</th><tr>"
+            for row in c.execute("SELECT * FROM equipement WHERE InsNumeroInstall LIKE \'%"+id+"%\'"):
+                for elem in row:
+                    maPage += "<td>" + str(elem) + "</td>"
+                maPage += "</tr>"
+            maPage += "</table>"
+        except (IndexError, IOError):
+            return "Invalid ID"
+
+        return maPage
+
+    @cherrypy.expose
+    def show_activite_id(self, id):
+        """
+        Exposes the service at localhost:8080/show_activite_id/[id]/
+        """
+        maPage = """<h1>Recherche d'installations sportives</h1>
+                    <h2>Recheche d'activitées sportives</h2>
+                    <form name='monForm' action='show_activite_id'>
+                        <input type="text" name='id'>
+                        <input type="submit">
+                        <a href='index'>Retour</a>
+                    </form>
+                    """
+        try:
+            conn = sqlite3.connect("dataBase.db")
+            c = conn.cursor()
+            maPage +="<table border='1' style='text-align'><th>ID Activite</th><th>Nom Activite</th><th>ID Equipement</th><tr>"
+            for row in c.execute("SELECT * FROM activite WHERE ActCode LIKE \'%"+id+"%\'"):
+                for elem in row:
+                    maPage += "<td>" + str(elem) + "</td>"
+                maPage += "</tr>"
+            maPage += "</table>"
+        except (IndexError, IOError):
+            return "Invalid ID"
+
+        return maPage
 
 
 cherrypy.quickstart(WebManager())
