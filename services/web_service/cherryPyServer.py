a52ef2688351994b3fe73f76a35af29f60399b58
hastaking <hastaking@gmail.com>
2015-04-01 17:15:02 +0200

    MAJ Readme - CSS UP

--- services/web_service/cherryPyServer.py
+++ services/web_service/cherryPyServer.py
@@ -14,36 +14,29 @@ class WebManager(object):
         Exposes the service at localhost:8080/
         """
         maPage = ""
-        mytemplate = Template(filename='services/web_service/template/header.html')
-        myothertemplate = Template(filename='services/web_service/template/index.html')
-        maPage += (mytemplate.render())
-        maPage += (myothertemplate.render())
+        mytemplate1 = Template(filename='services/web_service/template/header.html')
+        mytemplate2 = Template(filename='services/web_service/template/index.html')
+        mytemplate3 = Template(filename='services/web_service/template/footer.html')
+        maPage += (mytemplate1.render())
+        maPage += (mytemplate2.render())
+        maPage += (mytemplate3.render())
         return maPage
 
     @cherrypy.expose
-    def show_equipement(self, id, recherche):
+    def show_equipment(self, id, recherche):
         """
-        Exposes the service at localhost:8080/show_equipement?recherche=""&id=""
+        Exposes the service at localhost:8080/show_equipment?recherche=""&id=""
         """
-        maPage = """<h1>Recherche d'installations sportives</h1>
-                    <h2>Recheche d'équipements sportif</h2>
-                    <form name='monForm' action='show_equipement'>
-                        Rechercher par
-                        <select name='recherche'>
-                            <option value='EquNom'> Nom Equipement
-                            <option value='EquipementId'> ID Equipement
-                            <option value='InsNumeroInstall'> ID Installation
-                        </select>
-                        :
-                        <input type="text" name='id'>
-                        <input type="submit">
-                        <a href='index'>Retour</a>
-                    </form>
-                    """
+        maPage = ""
+        mytemplate1 = Template(filename='services/web_service/template/header.html')
+        mytemplate2 = Template(filename='services/web_service/template/equipment.html')
+        mytemplate3 = Template(filename='services/web_service/template/footer.html')
+        maPage += (mytemplate1.render())
+        maPage += (mytemplate2.render())
         try:
             conn = sqlite3.connect("dataBase.db")
             c = conn.cursor()
-            maPage +="<table border='1' style='text-align'><th>Nom</th><th>ID Equipement</th><th>ID Installation</th><tr>"
+            maPage +="<table class=\"table-striped\"><th>Nom</th><th>ID Equipement</th><th>ID Installation</th><tr>"
             for row in c.execute("SELECT * FROM equipement WHERE "+recherche+" LIKE \'%"+id+"%\'"):
                 for elem in row:
                     maPage += "<td>" + str(elem) + "</td>"
@@ -51,6 +44,7 @@ class WebManager(object):
             maPage += "</table>"
         except (IndexError, IOError):
             return "Invalid ID"
+        maPage += (mytemplate3.render())
 
         return maPage
 
@@ -59,29 +53,16 @@ class WebManager(object):
         """
         Exposes the service at localhost:8080/show_installation?recherche=""&id=""
         """
-        maPage = """<h1>Recherche d'installations sportives</h1>
-                    <h2>Recheche d'installations sportives</h2>
-                    <form name='monForm' action='show_installation'>
-                        Rechercher par 
-                        <select name='recherche'>
-                            <option value='Comlib'> Ville 
-                            <option value='InsCodePostal'> Code Postale
-                            <option value='InsLibelleVoie'> Adresse
-                            <option value='InsNumeroInstall'> ID Installation 
-                            <option value='InsPartLibelle'> Nom Installation
-                            <option value='Latitude'> Latitude
-                            <option value='Longitude'> Longitude
-                        </select>
-                        :
-                        <input type="text" name='id'>
-                        <input type="submit">
-                        <a href='index'>Retour</a>
-                    </form>
-                    """
+        maPage = ""
+        mytemplate1 = Template(filename='services/web_service/template/header.html')
+        mytemplate2 = Template(filename='services/web_service/template/installation.html')
+        mytemplate3 = Template(filename='services/web_service/template/footer.html')
+        maPage += (mytemplate1.render())
+        maPage += (mytemplate2.render())
         try:
             conn = sqlite3.connect("dataBase.db")
             c = conn.cursor()
-            maPage +="<table border='1'><th>Ville</th><th>Code Postal</th><th>Adresse</th><th>ID Installation</th><th>Nom Installation</th><th>Latitude</th><th>Longitude</th><tr>"
+            maPage +="<table><th>Ville</th><th>Code Postal</th><th>Adresse</th><th>ID Installation</th><th>Nom Installation</th><th>Latitude</th><th>Longitude</th><tr>"
             for row in c.execute("SELECT * FROM installation WHERE "+recherche+" LIKE \'%"+id+"%\'"):
                 for elem in row:
                     maPage += "<td>" + str(elem) + "</td>"
@@ -89,33 +70,25 @@ class WebManager(object):
             maPage += "</table>"
         except (IndexError, IOError):
             return "Invalid ID"
+        maPage += (mytemplate3.render())
 
         return maPage
 
     @cherrypy.expose
-    def show_activite(self, id, recherche):
+    def show_activity(self, id, recherche):
         """
-        Exposes the service at localhost:8080/show_activite?recherche=""&id=""
+        Exposes the service at localhost:8080/show_activity?recherche=""&id=""
         """
-        maPage = """<h1>Recherche d'installations sportives</h1>
-                    <h2>Recheche d'activitées sportives</h2>
-                    <form name='monForm' action='show_activite'>
-                        Rechercher par
-                        <select name='recherche'>
-                            <option value='ActCode'>ID Activité
-                            <option value='ActLib'> Nom Activité
-                            <option value='EquipementId'> ID Equipement
-                        </select>
-                        :
-                        <input type="text" name='id'>
-                        <input type="submit">
-                        <a href='index'>Retour</a>
-                    </form>
-                    """
+        maPage = ""
+        mytemplate1 = Template(filename='services/web_service/template/header.html')
+        mytemplate2 = Template(filename='services/web_service/template/activity.html')
+        mytemplate3 = Template(filename='services/web_service/template/footer.html')
+        maPage += (mytemplate1.render())
+        maPage += (mytemplate2.render())
         try:
             conn = sqlite3.connect("dataBase.db")
             c = conn.cursor()
-            maPage +="<table border='1' style='text-align'><th>ID Activite</th><th>Nom Activite</th><th>ID Equipement</th><tr>"
+            maPage +="<table><th>ID Activite</th><th>Nom Activite</th><th>ID Equipement</th><tr>"
             for row in c.execute("SELECT * FROM activite WHERE "+recherche+" LIKE \'%"+id+"%\'"):
                 for elem in row:
                     maPage += "<td>" + str(elem) + "</td>"
@@ -123,6 +96,7 @@ class WebManager(object):
             maPage += "</table>"
         except (IndexError, IOError):
             return "Invalid ID"
+        maPage += (mytemplate3.render())
 
         return maPage
 
